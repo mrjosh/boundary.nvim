5b6bf3c352a487d0334a9ce271918537a1d0b005
Josh <josheghani.dev@gmail.com>
2024-01-18 00:55:57 +0330

    [feat] update

--- plugin/plugin.lua
+++ plugin/plugin.lua
@@ -1,4 +1,4 @@
-require('boundary').setup({
+require('boundary.nvim').setup({
   boundary_addr = "http://localhost:9200",
   boundary_telescope_script_path = vim.fn.stdpath('config') .. "/plugged/mrjosh/boundary.nvim/scripts/boundary_telescope_cmd",
 })
